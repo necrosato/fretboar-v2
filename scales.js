85a2ac60f9651cec83de4807ca6bcbf72d6b0b2c
Naookie Sato <necrosato@live.com>
2025-06-07 17:46:48 -0700

    more scales

---
+++ scales.js
@@ -0,0 +1,54 @@
+// ONE-LINE SCALE DEFINITIONS – Add/remove from this list to modify available scales
+window.scaleDefs = [
+  // Western Traditional
+  ["major",              [0, 2, 4, 5, 7, 9, 11]],
+  ["natural_minor",      [0, 2, 3, 5, 7, 8, 10]],
+  ["harmonic_minor",     [0, 2, 3, 5, 7, 8, 11]],
+  ["melodic_minor",      [0, 2, 3, 5, 7, 9, 11]],
+  ["major_pentatonic",   [0, 2, 4, 7, 9]],
+  ["minor_pentatonic",   [0, 3, 5, 7, 10]],
+  ["blues",              [0, 3, 5, 6, 7, 10]],
+
+  // Modes of the major scale
+  ["ionian",             [0, 2, 4, 5, 7, 9, 11]],
+  ["dorian",             [0, 2, 3, 5, 7, 9, 10]],
+  ["phrygian",           [0, 1, 3, 5, 7, 8, 10]],
+  ["lydian",             [0, 2, 4, 6, 7, 9, 11]],
+  ["mixolydian",         [0, 2, 4, 5, 7, 9, 10]],
+  ["aeolian",            [0, 2, 3, 5, 7, 8, 10]],
+  ["locrian",            [0, 1, 3, 5, 6, 8, 10]],
+
+  // Symmetrical
+  ["diminished",         [0, 2, 3, 5, 6, 8, 9, 11]],
+  ["whole_tone",         [0, 2, 4, 6, 8, 10]],
+  ["chromatic",          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]],
+
+  // Exotic / World
+  ["double_harmonic",    [0, 1, 4, 5, 7, 8, 11]],
+  ["hungarian_minor",    [0, 2, 3, 6, 7, 8, 11]],
+  ["persian",            [0, 1, 4, 5, 6, 8, 11]],
+  ["arabian",            [0, 2, 4, 5, 6, 8, 10]],
+  ["egyptian",           [0, 2, 5, 7, 10]],
+  ["hindu",              [0, 2, 4, 5, 7, 8, 10]],
+  ["spanish_phrygian",   [0, 1, 4, 5, 7, 8, 10]],
+  ["romanian_minor",     [0, 2, 3, 6, 7, 9, 10]],
+  ["flamenco",           [0, 1, 4, 5, 7, 8, 10]],
+
+  // Japanese
+  ["hirajoshi",          [0, 2, 3, 7, 8]],
+  ["iwato",              [0, 1, 5, 6, 10]],
+  ["kumoi",              [0, 2, 3, 7, 9]],
+  ["insen",              [0, 1, 5, 7, 10]],
+  ["yo",                 [0, 2, 5, 7, 9]],
+  ["in",                 [0, 1, 5, 7, 8]],
+  ["ryukyu",             [0, 4, 5, 7, 11]],
+
+  // Others
+  ["bebop_major",        [0, 2, 4, 5, 7, 8, 9, 11]],
+  ["bebop_dominant",     [0, 2, 4, 5, 7, 9, 10, 11]],
+  ["lydian_dominant",    [0, 2, 4, 6, 7, 9, 10]],
+  ["half_diminished",    [0, 2, 3, 5, 6, 8, 10]],
+  ["augmented",          [0, 3, 4, 7, 8, 11]]
+];
+
+
